d61ec962ed8b6c2f7f6537a4b47285d7d50d85c5
Michel Kim <kim98.michel@gmail.com>
2023-05-03 00:39:48 -0300

    add logic to avoid repeating the same word

--- app/src/main/java/br/com/homegame/MainActivity.kt
+++ app/src/main/java/br/com/homegame/MainActivity.kt
@@ -8,21 +8,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val lastWord: String = wordList.random()
-
-        binding.gameText.text = lastWord
-
-        binding.gameNextButton.setOnClickListener {
-            binding.gameText.text = wordList.random()
-        }
-    }
-
-    val wordList: List<String> = listOf(
+    private val wordList: List<String> = listOf(
         "Abacate",
         "Girassol",
         "Chocolate",
@@ -34,4 +20,25 @@ class MainActivity : AppCompatActivity() {
         "Montanha",
         "Palmeira"
     )
+    private val lastWord: String = wordList.random()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.gameText.text = lastWord
+
+        binding.gameNextButton.setOnClickListener {
+            binding.gameText.text = showNextWord(wordList, lastWord)
+        }
+    }
+
+    private fun showNextWord(wordList: List<String>, lastWord: String): String {
+        var word: String
+        do {
+            word = wordList.random()
+        } while (word == lastWord)
+        return word
+    }
 }
